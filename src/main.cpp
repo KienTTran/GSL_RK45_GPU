8cc1bd6e13a395058e0773dd90103e9fb9925b92
KienTTran <ktt5121@psu.edu>
2022-01-12 12:00:08 -0500

    test cuda on server

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@ int main()
     rk45_cpu_simulate(threads,display);
     std::cout << std::endl;
     std::cout << "Performing test 1 on GPU" << std::endl;
-    test_cuda_1();
+    test_cuda_2();
     std::cout << std::endl;
     std::cout << "Performing test 2 on GPU" << std::endl;
-    test_cuda_2();
+    test_cuda_1();
     std::cout << std::endl;
     std::cout << "Running GSL on GPU" << std::endl;
     rk45_gpu_simulate(threads,display);
